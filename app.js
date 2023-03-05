30ed2ebfa948303477ff3d9021e6fed81610b95f
Anıl DEMİR <anil.demir@botas.dom>
2023-03-05 14:43:30 +0300

    finished app

---
+++ app.js
@@ -0,0 +1,35 @@
+const progress = document.getElementById("progress");
+const circles = document.querySelectorAll(".circle");
+const next = document.getElementById("next");
+const prev = document.getElementById("prev");
+let actives = 0;
+let currentActive = 1;
+next.addEventListener("click", () => {
+  currentActive++;
+  if (currentActive > circles.length) {
+    currentActive = circles.length;
+  }
+  update();
+});
+
+prev.addEventListener("click", () => {
+  currentActive--;
+  currentActive < 1 && (currentActive = 1);
+  update();
+});
+
+function update() {
+  circles.forEach((circle, idx) => {
+    if (idx < currentActive) {
+      circle.classList.add("active");
+    } else {
+      circle.classList.remove("active");
+    }
+    actives = document.querySelectorAll(".active");
+  });
+
+  console.log(((actives.length - 1) / (circles.length - 1)) * 100);
+  progress.style.width = `${
+    ((actives.length - 1) / (circles.length - 1)) * 100
+  }%`;
+}
